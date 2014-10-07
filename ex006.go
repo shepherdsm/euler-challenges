ec75945dd0fbaef86a786d3fe7c051afde4f8b56
Scott Shepherd <scottmshepherd@gmail.com>
2014-10-07 18:56:45 -0400

    Updated solution.

--- ex006.go
+++ ex006.go
@@ -6,17 +6,35 @@ import "fmt"
 // projecteuler.net/problem=6
 func main() {
     fmt.Printf("Solution 1: %d\n", solve1())
+    fmt.Printf("Solution 2: %d\n", solve2())
+}
+
+func sumTheSquares (upper int) (sum int) {
+    for i := 1; i <= upper; i++ {
+        sum += i * i
+    }
+
+    return
+}
+
+func squareTheSum (upper int) (int) {
+    return (upper * upper * (upper + 1) * (upper + 1))/ 4
 }
 
 func solve1() (int) {
     upper := 100
-    sumSquares := 0
 
-    for i := 1; i <= upper; i++ {
-        sumSquares += i * i
-    }
+    sumSquares := sumTheSquares(upper)
+    squareSum := squareTheSum(upper)
 
-    squareSum := (upper * (upper + 1)) / 2
+    return squareSum - sumSquares
+}
 
-    return squareSum * squareSum - sumSquares
+func solve2() (int) {
+    upper := 100
+
+    sumSquares := (upper * (2 * upper + 1) * (upper + 1)) / 6
+    squareSum := squareTheSum(upper)
+
+    return squareSum - sumSquares
 }
